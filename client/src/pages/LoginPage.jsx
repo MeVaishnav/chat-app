5207d26aef6118cca2e250f9bd9f55c464a82fed
MeVaishnav <vaishnavcsbedradka@gmail.com>
2026-02-03 20:49:31 +0530

    implemented login and signup page

--- client/src/pages/LoginPage.jsx
+++ client/src/pages/LoginPage.jsx
@@ -8,6 +8,15 @@ const LoginPage = () => {
   const [password, setPassword] = useState("");
   const [bio, setBio] = useState("");
   const [isDataSubmitted, setIsDataSubmitted] = useState(false);
+
+  const onSubmitHandler = (e) => {
+    e.preventDefault();
+    if (currentState === "Sign up" && !isDataSubmitted) {
+      setIsDataSubmitted(true);
+      return;
+    }
+  };
+
   return (
     <div className="min-h-screen bg-cover bg-center flex items-center justify-center gap-8 sm:justify-evenly max-sm:flex-col backdrop-blur-2xl">
       {/* Left Section */}
@@ -15,13 +24,25 @@ const LoginPage = () => {
 
       {/* -------- right -------- */}
 
-      <form className="border-2 bg-white/8 text-white border-gray-500 p-6 flex flex-col gap-6 rounded-lg shadow-lg">
+      <form
+        onSubmit={onSubmitHandler}
+        className="border-2 bg-white/8 text-white border-gray-500 p-6 flex flex-col gap-6 rounded-lg shadow-lg"
+      >
         <h2 className="font-medium text-2xl flex justify-between items-center">
           {currentState}
-          <img src={assets.arrow_icon} alt="" className="w-5 cursor-pointer" />
+          {isDataSubmitted && (
+            <img
+              onClick={() => setIsDataSubmitted(false)}
+              src={assets.arrow_icon}
+              alt=""
+              className="w-5 cursor-pointer"
+            />
+          )}
         </h2>
         {currentState === "Sign up" && !isDataSubmitted && (
           <input
+            onChange={(e) => setFullName(e.target.value)}
+            value={fullName}
             type="text"
             className="p-2 border border-gray-500 rounded-md focus:outline-none focus:ring-2 focus:ring-indigo-500"
             placeholder="FullName"
@@ -32,13 +53,74 @@ const LoginPage = () => {
         {!isDataSubmitted && (
           <>
             <input
+              onChange={(e) => setEmail(e.target.value)}
+              value={email}
               type="email"
               placeholder="Email Address"
               required
               className="p-2 border border-gray-500 rounded-md focus:outline-none focus:ring-2 focus:ring-indigo-500"
             />
+
+            <input
+              onChange={(e) => setPassword(e.target.value)}
+              value={password}
+              type="password"
+              placeholder="Password"
+              required
+              className="p-2 border border-gray-500 rounded-md focus:outline-none focus:ring-2 focus:ring-indigo-500"
+            />
           </>
         )}
+
+        {currentState === "Sign up" && isDataSubmitted && (
+          <textarea
+            onChange={(e) => setBio(e.target.value)}
+            value={bio}
+            rows={4}
+            className="p-2 border border-gray-500 rounded-md focus:outline-none focus:ring-2 focus:ring-indigo-500"
+            placeholder="Provide a Bio... "
+            required
+          ></textarea>
+        )}
+
+        <button
+          type="submit"
+          className="py-3 bg-gradient-to-r from-purple-400 to-violet-600 text-white rounded-md cursor-pointer"
+        >
+          {currentState === "Sign up" ? "Create Account" : "Login"}
+        </button>
+
+        <div className="flex justify-between text-sm text-gray-300">
+          <input type="checkbox" />
+          <p>I agree to the terms and conditions</p>
+        </div>
+
+        <div className="flex flex-col gap-2">
+          {currentState === "Sign up" ? (
+            <p className="text-sm text-gray-600">
+              Already have an account?
+              <span
+                onClick={() => {
+                  setCurrentState("Login");
+                  setIsDataSubmitted(false);
+                }}
+                className="font-medium text-violet-500 cursor-pointer"
+              >
+                Login here
+              </span>
+            </p>
+          ) : (
+            <p className="text-sm text-gray-600">
+              Create an account
+              <span
+                className="font-medium text-violet-500 cursor-pointer"
+                onClick={() => setCurrentState("Sign up")}
+              >
+                Click here
+              </span>
+            </p>
+          )}
+        </div>
       </form>
     </div>
   );
